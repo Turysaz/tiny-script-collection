b4140e7223ef2209a5a800682305f90ef5c4614f
Turysaz <turysaz@users.noreply.github.com>
2016-10-13 20:26:04 +0200

    Begin variable name with _underscore

--- texmake.sh
+++ texmake.sh
@@ -3,11 +3,11 @@
 # renders tex project
 # requires nomencl.ist to be within the directory
 
-file=${1:-main}
+_file=${1:-main}
 
-pdflatex $file.tex
-makeindex $file.nlo -s nomencl.ist -o $file.nls
-bibtex $file
-pdflatex $file.tex
-pdflatex $file.tex
+pdflatex $_file.tex
+makeindex $_file.nlo -s nomencl.ist -o $_file.nls
+bibtex $_file
+pdflatex $_file.tex
+pdflatex $_file.tex
 
