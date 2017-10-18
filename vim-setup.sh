c62218a188cdceb6d4260408985dba01b9452e56
Turysaz <turysaz@posteo.org>
2017-10-18 13:43:53 +0200

    Improve vim-setup

--- vim-setup.sh
+++ vim-setup.sh
@@ -2,24 +2,29 @@
 
 td=temp #tempdir
 
+echo creating temporary directory
 mkdir $td
 cd $td
 
+echo ""
 echo "cloning airline"
 echo "==============="
 
 git clone https://github.com/vim-airline/vim-airline.git
 
+echo ""
 echo "cloning airline themes"
 echo "======================"
 git clone https://github.com/vim-airline/vim-airline-themes.git
 
+echo ""
 echo "cloning git branch info"
 echo "======================="
 git clone https://github.com/taq/vim-git-branch-info.git
 
-echo
-echo
+echo ""
+echo "start installation"
+echo ""
 
 echo "installing airline"
 rsync vim-airline/ ~/.vim/
@@ -33,4 +38,14 @@ rsync vim-airline/ ~/.vim/
 echo "install vimrc"
 cp ../_vimrc-default ~/.vimrc
 
+echo
+echo "installation complete"
+echo
+
+echo "removing temp dir"
+cd ..
+rm -rf $td
+
+echo "NERDtree sill missing :("
+echo "done."
 
