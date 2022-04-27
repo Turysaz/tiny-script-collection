ac19d2e18d969e4cc60297b090301a2bb8631720
Turysaz <turysaz@posteo.org>
2022-04-27 23:53:38 +0200

    Add conflict resolving to bash functions

--- conf/bash/_bash_functions.sh
+++ conf/bash/_bash_functions.sh
@@ -100,3 +100,55 @@ function randhex
     echo $RANDOM | xargs printf "%04x"
 }
 
+
+# resolve init
+__conflicts-init() {
+    # read the names of all unmerged files into the array _CONFLICTS
+    readarray -t _CONFLICTS < <(git diff --name-only --diff-filter=U)
+}
+
+# list all conflicts
+conflicts-list() {
+    __conflicts-init
+
+    max=${#_CONFLICTS[@]}
+    if [[ $max -le 0 ]]
+    then
+        echo "No conflicting files known."
+        return
+    fi
+
+    for i in `seq 0 $(($max - 1))`
+    do
+        printf '%i: %s\n' $i ${_CONFLICTS[$i]}
+    done
+}
+
+# open file with conflicts
+conflicts-edit() {
+    re='^[0-9]+$'
+
+    __conflicts-init
+
+    if [[ ${#_CONFLICTS} -le 0 ]]
+    then
+        echo "No conflicting files known."
+        return
+    fi
+
+    if [[ -z $1 ]]
+    then
+        echo "Please specify the file's index."
+        echo "(Run 'conflicts-list' to get a list.)"
+        return
+    fi
+
+    if [[ ! $1 =~ $re || $1 -ge ${#_CONFLICTS[@]} || $1 -lt 0 ]]
+    then
+        echo "Invalid index: '$1'"
+        return
+    fi
+
+    vim "${_CONFLICTS[$1]}"
+}
+
